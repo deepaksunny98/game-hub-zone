de1b60c9c9acefeed47cf689e0ae0ec1b03d213f
deepaksunny98 <deepak.sunny98@gmail.com>
2025-04-11 15:00:18 -0400

    optimizing Images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Games } from "../services/games-service";
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImaheUrl from "../services/image-url";
 
 interface Props {
 	game: Games;
@@ -14,7 +15,7 @@ const GameCard = ({ game }: Props) => {
 			borderRadius={10}
 			overflow={"hidden"}
 		>
-			<Image src={game.background_image} />
+			<Image src={getCroppedImaheUrl(game.background_image)} />
 			<CardBody>
 				<Heading fontSize='2xl'>{game.name}</Heading>
 				<HStack justifyContent='space-between'>
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -34,6 +34,7 @@ const PlatformIconList = ({ platforms }: Props) => {
 		<HStack marginY={1}>
 			{platforms.map((platform) => (
 				<Icon
+					key={platform.id}
 					as={iconMap[platform.slug]}
 					color='gray.500'
 				/>
