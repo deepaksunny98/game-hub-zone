98f2696e8089b81f572ddcb63642dfdc478e2416
deepaksunny98 <deepak.sunny98@gmail.com>
2025-05-08 22:01:19 -0400

    Expandanle text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,34 @@
+import { Button, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface Props {
+	children: string;
+}
+
+const ExpandableText = ({ children }: Props) => {
+	const [isExpanded, setExpanded] = useState(false);
+	const limit = 300;
+
+	if (children.length <= limit) return <Text>{children}</Text>;
+
+	const expandedText = isExpanded ? children : `${children.slice(0, limit)}...`;
+
+	return (
+		<>
+			<Text>
+				{expandedText}
+				<Button
+					onClick={() => setExpanded(!isExpanded)}
+					colorScheme='yellow'
+					fontWeight={"bold"}
+					size={"xs"}
+					marginLeft={1}
+				>
+					{isExpanded ? "Show less" : "Read more"}
+				</Button>
+			</Text>
+		</>
+	);
+};
+
+export default ExpandableText;
--- src/components/GameDetailPage.tsx
+++ src/components/GameDetailPage.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import { Heading, Text } from "@chakra-ui/react";
 import useGameDetails from "../hooks/useGameDetails";
 import { useParams } from "react-router-dom";
+import ExpandableText from "./ExpandableText";
 
 const GameDetailPage = () => {
 	const { game_name } = useParams();
 
 	const { data: gameDetail, error } = useGameDetails(game_name!);
 
+	const gameDescription = gameDetail?.description_raw;
+
 	if (error) return <Text>{error.message}</Text>;
 
 	return (
 		<>
 			<Heading>{gameDetail?.name}</Heading>
-			<Text>{gameDetail?.description_raw}</Text>
+			<ExpandableText>{gameDescription || ""}</ExpandableText>
 		</>
 	);
 };
